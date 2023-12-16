e45578a9605d527d075dc7aa4ddf052819164bf4
emmanuelogoma <eogoma@safaricom.co.ke>
2023-12-16 13:29:47 +0300

    Added @GetMapping

--- openshift/openshift-demo/src/main/java/com/ogoma/openshiftdemo/HelloWorldController.java
+++ openshift/openshift-demo/src/main/java/com/ogoma/openshiftdemo/HelloWorldController.java
@@ -1,14 +1,15 @@
 package com.ogoma.openshiftdemo;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/greeting")
 public class HelloWorldController {
-
-    public ResponseEntity greeting(){
+    @GetMapping
+    public ResponseEntity greeting() {
         return ResponseEntity.ok("Hello World!");
     }
 }
