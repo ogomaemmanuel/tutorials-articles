46acce55287310b3e2b4d31f1e331488a4293a1e
emmanuelogoma <eogoma@safaricom.co.ke>
2023-12-16 12:02:42 +0300

    first commit

---
+++ openshift/openshift-demo/src/main/java/com/ogoma/openshiftdemo/OpenshiftDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ogoma.openshiftdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenshiftDemoApplication.class, args);
+    }
+
+}
---
+++ openshift/openshift-demo/src/test/java/com/ogoma/openshiftdemo/OpenshiftDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogoma.openshiftdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenshiftDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
